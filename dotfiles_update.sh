4ceca9e400d4edf31e92a7389fca234716cc896d
Krzysztof Skórcz <krzysztof.skorcz@inverto.tv>
2019-10-02 10:02:14 +0200

    Update tmux config

--- dotfiles_update.sh
+++ dotfiles_update.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
 cat $HOME/.bashrc.ubuntu.local >> $HOME/bashrc
+cat $HOME/.tmux.conf.ubuntu >> $HOME/.tmux.conf
 cat $HOME/.config/ranger/rc.conf.local >> $HOME/.config/ranger/rc.conf
