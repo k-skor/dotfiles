1d95d0707dbdc5adab1758e07d27ebd324c1ba74
Krzysztof Skórcz <krzy.skorcz@gmail.com>
2020-03-17 17:58:28 +0000

    Update to Python3

--- dotfiles_init.sh
+++ dotfiles_init.sh
@@ -4,7 +4,6 @@ config='/usr/bin/git --git-dir='$HOME'/.cfg/ --work-tree='$HOME
 echo ".cfg" >> .gitignore
 git clone --bare https://github.com/k-skor/dotfiles.git $HOME/.cfg
 $config config --local status.showUntrackedFiles no
-echo "alias config='$config'" >> $HOME/.bashrc
 
 mkdir -p .config-backup && \
 config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
@@ -12,3 +11,5 @@ xargs -I{} mv {} .config-backup/{}
 res=${PIPESTATUS[0]}
 echo "Repository checkout result: $res"
 if [ $res -ne 0 ]; then config checkout; fi
+
+echo "alias config='$config'" >> $HOME/.bashrc
