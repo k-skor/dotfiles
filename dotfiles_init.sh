03cbd70c20f99d0db7f0bb6042e226472f1f98ee
k-skor <krzy.skorcz@gmail.com>
2020-03-17 15:35:30 +0100

    Update dotfiles_init.sh

--- dotfiles_init.sh
+++ dotfiles_init.sh
@@ -5,3 +5,10 @@ echo ".cfg" >> .gitignore
 git clone --bare https://github.com/k-skor/dotfiles.git $HOME/.cfg
 $config config --local status.showUntrackedFiles no
 echo "alias config='$config'" >> $HOME/.bashrc
+
+mkdir -p .config-backup && \
+config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} .config-backup/{}
+res=${PIPESTATUS[0]}
+echo "Repository checkout result: $res"
+if [ $res -ne 0 ]; then config checkout; fi
