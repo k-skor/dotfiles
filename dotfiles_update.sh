b10d5d3767a97de3d85fc3a3e00c93bc374958c9
Krzysztof Skórcz <krzysztof.skorcz@capgemini.com>
2020-03-17 13:47:38 +0100

    Source config from base file

--- dotfiles_update.sh
+++ dotfiles_update.sh
@@ -1,5 +1,2 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-cat $HOME/.bashrc.ubuntu.local >> $HOME/bashrc
-cat $HOME/.tmux.conf.ubuntu >> $HOME/.tmux.conf
-cat $HOME/.config/ranger/rc.conf.local >> $HOME/.config/ranger/rc.conf
